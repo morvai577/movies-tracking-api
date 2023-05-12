1bc63fdf625b90835e9b8c3a20ae7ab8add9e7d5
Vaibhav Mor <thevaimor@icloud.com>
2023-05-12 16:32:08 +1200

    Add mappings for res and update movie req

--- Movies.Api/Mapping/ContractMapping.cs
+++ Movies.Api/Mapping/ContractMapping.cs
@@ -1,5 +1,6 @@
 using Movies.Application.Models;
 using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
 
 namespace Movies.Api.Mapping;
 
@@ -13,6 +14,36 @@ public static class ContractMapping
             Title = request.Title,
             YearOfRelease = request.YearOfRelease,
             Genres = request.Genres.ToList()
+        };
+    }
+
+    public static Movie MapToMovie(this UpdateMovieRequest request, Guid id)
+    {
+        return new()
+        {
+            Id = id,
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
+    }
+
+    public static MovieResponse MapToResponse(this Movie movie)
+    {
+        return new()
+        {
+            Id = movie.Id,
+            Title = movie.Title,
+            YearOfRelease = movie.YearOfRelease,
+            Genres = movie.Genres
+        };
+    }
+
+    public static MoviesResponse MapToResponse(this IEnumerable<Movie> movies)
+    {
+        return new()
+        {
+            Items = movies.Select(MapToResponse)
         };
     }
 }
