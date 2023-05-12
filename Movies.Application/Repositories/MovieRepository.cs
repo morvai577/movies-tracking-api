b34fdd68bba56610190d66fff8cc446a991bb133
Vaibhav Mor <thevaimor@icloud.com>
2023-05-12 20:12:41 +1200

    Finish updating movie repo to use db ops

--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -38,33 +38,124 @@ public class MovieRepository : IMovieRepository
         return result > 0;
     }
 
-    public Task<Movie?> GetByIdAsync(Guid id)
+    public async Task<Movie?> GetByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var movie = await connection.QuerySingleOrDefaultAsync<Movie>(
+            new CommandDefinition("""
+                select * from movies where id = @id;
+            """, new { id }));
+
+        if (movie is null)
+        {
+            return null;
+        }
+
+        var genres = await connection.QueryAsync<string>(
+            new CommandDefinition("""
+            select name from genres where movieId = @id;
+            """, new { id }));
+
+        foreach (var genre in genres)
+        {
+            movie.Genres.Add(genre);
+        }
+
+        return movie;
     }
 
-    public Task<IEnumerable<Movie>> GetAllAsync()
+    public async Task<IEnumerable<Movie>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var result = await connection.QueryAsync(new CommandDefinition("""
+            select m.*, string_agg(g.name, ',') as genres
+            from movies m left join genres g on m.id = g.movieId
+            group by m.id;
+        """));
+
+        return result.Select(x => new Movie
+        {
+            Id = x.id,
+            Title = x.title,
+            YearOfRelease = x.yearofrelease,
+            Genres = Enumerable.ToList(x.genres.Split(',')),
+        });
     }
 
-    public Task<bool> UpdateAsync(Movie movie)
+    public async Task<bool> UpdateAsync(Movie movie)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        using var transaction = connection.BeginTransaction();
+
+        await connection.ExecuteAsync(new CommandDefinition("""
+            delete from genres where movieId = @id;
+            """, new { id = movie.Id }));
+        
+        foreach (var genre in movie.Genres)
+        {
+            await connection.ExecuteAsync(new CommandDefinition("""
+                insert into genres (movieId, name)
+                values (@MovieId, @Name);
+            """, new { MovieId = movie.Id, Name = genre }));
+        }
+
+        var result = await connection.ExecuteAsync(new CommandDefinition("""
+            update movies set slug = @Slug, title = @Title, yearofrelease = @YearOfRelease
+            where id = @Id;
+            """, movie));
+
+        transaction.Commit();
+        return result > 0;
     }
 
-    public Task<bool> DeleteByIdAsync(Guid id)
+    public async Task<bool> DeleteByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        using var transaction = connection.BeginTransaction();
+
+        await connection.ExecuteAsync(new CommandDefinition("""
+            delete from genres where movieId = @id;
+            """, new { id }));
+
+        var result = await connection.ExecuteAsync(new CommandDefinition("""
+            delete from movies where id = @id;
+            """, new { id }));
+
+        transaction.Commit();
+        return result > 0;
     }
 
-    Task<Movie?> IMovieRepository.GetBySlugAsync(string slug)
+    public async Task<Movie?> GetBySlugAsync(string slug)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        var movie = await connection.QuerySingleOrDefaultAsync<Movie>(
+            new CommandDefinition("""
+                select * from movies where slug = @slug;
+            """, new { slug }));
+
+        if (movie is null)
+        {
+            return null;
+        }
+
+        var genres = await connection.QueryAsync<string>(
+            new CommandDefinition("""
+            select name from genres where movieId = @id;
+            """, new { id = movie.Id }));
+
+        foreach (var genre in genres)
+        {
+            movie.Genres.Add(genre);
+        }
+
+        return movie;
     }
 
-    Task<bool> IMovieRepository.ExistsByIdAsync(Guid id)
+    public async Task<bool> ExistsByIdAsync(Guid id)
     {
-        throw new NotImplementedException();
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+        return await connection.ExecuteScalarAsync<bool>(new CommandDefinition("""
+            select count(1) from movies where id = @id;
+            """, new { id }));
     }
 }
