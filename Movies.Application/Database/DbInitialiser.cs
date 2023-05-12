8f03568697dc34975bc7d44f60516ee604171e98
Vaibhav Mor <thevaimor@icloud.com>
2023-05-12 17:44:34 +1200

    Add DbInitialiser for initialising db

---
+++ Movies.Application/Database/DbInitialiser.cs
@@ -0,0 +1,34 @@
+using Dapper;
+
+namespace Movies.Application.Database;
+
+public class DbInitialiser
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+    public DbInitialiser(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+    public async Task InitializeAsync()
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync();
+
+        // Create movies table if it doesn't exist
+        await connection.ExecuteAsync("""
+            create table if not exists movies (
+                id UUID primary key,
+                slug TEXT not null,
+                title TEXT not null,
+                yearofrelease integer not null);
+        """);
+
+        // Create unique index for lookup by slug
+        await connection.ExecuteAsync("""
+            create unique index concurrently if not exists movies_slug_idx
+            on movies
+            using btree(slug);
+        """);
+
+
+    }
+}
