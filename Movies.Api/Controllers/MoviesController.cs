2596b00782a739a1a9ccbfd6dcfa9c06911da561
Vaibhav Mor <thevaimor@icloud.com>
2023-05-12 15:46:06 +1200

    Create MoviesController.cs

---
+++ Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+[Route("api")]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost("movies")]
+    public async Task<IActionResult> Create([FromBody]CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"api/movies/{movie.Id}", movie);
+    }
+}
